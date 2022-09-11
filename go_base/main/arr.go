7651031019de6ce2c6bbd61a7a39109bc8769c56
vitaszhuo <vitaszhuo@xiaoe-teach.com>
2022-09-11 13:53:54 +0800

    提交最新代码

--- go_base/main/arr.go
+++ go_base/main/arr.go
@@ -1,27 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
-	"strings"
 )
 
-func main(){
+func main() {
 	//fmt.Println(91/10)
 
 	//测试break
-	for i:=0; i<2; i++{
-		j:=0
-		for {
-			if j>=5{
-				break
-			}
-			j++
-			fmt.Println("第",i,"列")
-		}
-	}
+	//for i := 0; i < 2; i++ {
+	//	j := 0
+	//	for {
+	//		if j >= 5 {
+	//			break
+	//		}
+	//		j++
+	//		fmt.Println("第", i, "列")
+	//	}
+	//}
 
 
+	jsons := `["1：修复直播黑屏，app闪退","2：优化ui"]`
+	var msg  []string
+	err := json.Unmarshal([]byte(jsons), &msg)
+
+	fmt.Println(msg , err)
 
 
 
@@ -54,7 +59,6 @@ func main(){
 
 	//1:数组的地址可以通过数组名来获取&intArr
 	//2.
-
 
 	//var score [5]float64
 	//
@@ -106,87 +110,79 @@ func main(){
 	//
 	//fmt.Printf("输出值%c",b)
 
-
 	//数组平均值
-//	var intArr = [...]int{1,-1,9,90,11}
-//	sum := 0
-//	for _,v := range intArr{
-//		//累计求和
-//		sum += v
-//	}
-//
-//	avg := float64(sum) /float64(len(intArr))
-//	fmt.Printf("sum=%v abg=%v",sum,avg)
+	//	var intArr = [...]int{1,-1,9,90,11}
+	//	sum := 0
+	//	for _,v := range intArr{
+	//		//累计求和
+	//		sum += v
+	//	}
+	//
+	//	avg := float64(sum) /float64(len(intArr))
+	//	fmt.Printf("sum=%v abg=%v",sum,avg)
+
+	//var intArr3 [5]int
+	////为了每次生成的随机数不一样，我们需要给一个seed值
+	//rand.Seed(time.Now().UnixNano())
+	//len := len(intArr3)
+	//for i:=0;i<len;i++{
+	//	intArr3[i] = rand.Intn(100)
+	//}
+	//fmt.Println(intArr3)
+	////反转打印
+	//temp :=0
+	//for i:=0;i<len/2;i++{
+	//	temp = intArr3[i]
+	//	intArr3[i] = intArr3[len - i -1]
+	//	intArr3[len - i -1] = temp
+	//}
+	//fmt.Println(intArr3)
+
+	//二维数组
+
+	//定义二位数组
+	//var arr [4][6]int
+	////fmt.Println(arr)
+	//
+	//for i:=0;i<len(arr);i++{
+	//	for j:=0;j<len(arr[i]);j++{
+	//		fmt.Print(" "+strconv.Itoa(arr[i][j]))
+	//	}
+	//	fmt.Println()
+	//}
+
+	//初始化数组
+	//var arr = [2][3]int{{1,2,3},{4,5,6}}
+	//fmt.Println("arr=",arr)
+	//
+	//data := []string{"apppcHqlTPT3482","13066867190"}
+	//fmt.Println(data)
+	//
+	////二维数组遍历
+	//for i,v := range arr{
+	//	for j,v2 := range v{
+	//		fmt.Printf("arr[%v][%v]=%v ",i,j,v2)
+	//	}
+	//	fmt.Println()
+	//}
+	//
+	//	params := genSQLRangeStrByIntArr([]string{"123","13066867190"})
+	//	fmt.Println(params)
+	//	s :=  fmt.Sprintf("SELECT * FROM t_phone_subscribe_record_0 WHERE (app_id=? AND phone_name=?)" ,params)
+	//	fmt.Println(s)
+	//}
+
+	//
+	//
+	//func genSQLRangeStrByIntArr(arr []string) (res string) {
+	//	var tempStrArr = make([]string, len(arr))
+	//	for k, v := range arr {
+	//		tempStrArr[k] = fmt.Sprintf("%s", v)
+	//	}
+	//	res = "(" + strings.Join(tempStrArr, ",") + ")"
+	//	return
+	//}
+	//
 
 
-//var intArr3 [5]int
-////为了每次生成的随机数不一样，我们需要给一个seed值
-//rand.Seed(time.Now().UnixNano())
-//len := len(intArr3)
-//for i:=0;i<len;i++{
-//	intArr3[i] = rand.Intn(100)
-//}
-//fmt.Println(intArr3)
-////反转打印
-//temp :=0
-//for i:=0;i<len/2;i++{
-//	temp = intArr3[i]
-//	intArr3[i] = intArr3[len - i -1]
-//	intArr3[len - i -1] = temp
-//}
-//fmt.Println(intArr3)
-
-//二维数组
-
-//定义二位数组
-//var arr [4][6]int
-////fmt.Println(arr)
-//
-//for i:=0;i<len(arr);i++{
-//	for j:=0;j<len(arr[i]);j++{
-//		fmt.Print(" "+strconv.Itoa(arr[i][j]))
-//	}
-//	fmt.Println()
-//}
-
-
-
-//初始化数组
-var arr = [2][3]int{{1,2,3},{4,5,6}}
-fmt.Println("arr=",arr)
-
-data := []string{"apppcHqlTPT3482","13066867190"}
-fmt.Println(data)
-
-//二维数组遍历
-for i,v := range arr{
-	for j,v2 := range v{
-		fmt.Printf("arr[%v][%v]=%v ",i,j,v2)
-	}
-	fmt.Println()
 }
-
-	params := genSQLRangeStrByIntArr([]string{"123","13066867190"})
-	fmt.Println(params)
-	s :=  fmt.Sprintf("SELECT * FROM t_phone_subscribe_record_0 WHERE (app_id=? AND phone_name=?)" ,params)
-	fmt.Println(s)
-}
-
-
-
-
-func genSQLRangeStrByIntArr(arr []string) (res string) {
-	var tempStrArr = make([]string, len(arr))
-	for k, v := range arr {
-		tempStrArr[k] = fmt.Sprintf("%s", v)
-	}
-	res = "(" + strings.Join(tempStrArr, ",") + ")"
-	return
-}
-
-
-
-
-
-
-
