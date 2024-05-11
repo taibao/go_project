16fb92efe8a8c6dc5a65a04e326264425b4cfe10
vitas <vitaszhuo@xiaoe-tech.com>
2024-05-11 11:10:34 +0800

    泛型

--- go_base/main/fanxing.go
+++ go_base/main/fanxing.go
@@ -21,12 +21,23 @@ func (s *Stack[T]) Push(x T) {
 	s.data = append(s.data, x)
 }
 
-//出栈
+// 出栈
+func (s *Stack[T]) Pop() T {
+	n := len(s.data)
+	x := s.data[n-1]
+	s.data = s.data[:n-1]
+	return x
+}
 
 func main() {
-	strings := toSlice("hello", "world")
-	fmt.Println("strings", strings)
-	nums := toSlice(1, 2, 3, 4)
-	fmt.Println("nums", nums)
-
+	//strings := toSlice("hello", "world")
+	//fmt.Println("strings", strings)
+	//nums := toSlice(1, 2, 3, 4)
+	//fmt.Println("nums", nums)
+	s := Stack[int]{}
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	x := s.Pop() //出栈
+	fmt.Println(x)
 }
