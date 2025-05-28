63dd2dbc1d1c0e6d97e27b234b7399b1ef9117d3
vitas <vitaszhuo@xiaoe-tech.com>
2025-05-28 22:05:25 +0800

    添加

--- go-admin-master/app/admin/models/sys_device.go
+++ go-admin-master/app/admin/models/sys_device.go
@@ -6,11 +6,10 @@ import (
 
 type SysDevice struct {
 	models.Model
-
-	DeviceName string `json:"device_name" gorm:"type:varchar(64);comment:设备名称"`
-	DeviceSn   string `json:"device_sn" gorm:"type:varchar(64);comment:设备sn号"`
-	ImgUrl     string `json:"img_url" gorm:"type:int;comment:图片链接"`
-	UserId     string `json:"user_id" gorm:"type:varchar(128);comment:用户编号"`
+	DeviceName string `json:"deviceName" gorm:"type:varchar(64);comment:设备名称"`
+	DeviceSn   string `json:"deviceSn" gorm:"type:varchar(64);comment:设备sn号"`
+	ImgUrl     string `json:"imgUrl" gorm:"type:int;comment:图片链接"`
+	UserId     string `json:"userId" gorm:"type:varchar(128);comment:用户编号"`
 	Status     string `json:"status" gorm:"type:varchar(4);comment:状态 0:正常 1：停用 2：删除"`
 	models.ModelTime
 	models.ControlBy
